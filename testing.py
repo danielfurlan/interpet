8dd35fa47347f823bc421a15ec8e3886c5e6faa0
Daniel F.M. Furlan <43858273+danielfurlan@users.noreply.github.com>
2020-12-05 16:56:43 -0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,36 @@
-return 0
+!pip install pyyaml==5.1
+!pip install 
+!pip install torch==1.7.0+cpu torchvision==0.8.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+!pip install cython
+
+import torch, torchvision
+print(torch.__version__, torch.cuda.is_available())
+!gcc --version
+# opencv is pre-installed on colab
+
+# install detectron2: (Colab has CUDA 10.1 + torch 1.7)
+# See https://detectron2.readthedocs.io/tutorials/install.html for instructions
+import torch
+assert torch.__version__.startswith("1.7")
+!pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.7/index.html
+# exit(0)  # After installation, you need to "restart runtime" in Colab. This line can also restart runtime
+
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+# from google.colab.patches import cv2_imshow
+import cv2
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+print("we managed to install!!")
